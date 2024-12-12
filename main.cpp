32648e5fe19a6c012379a806119c6f738a1cf476
GIKIStudent101 <u2024374@giki.edu.pk>
2024-12-12 11:13:48 +0500

    Update main.cpp
    Basic framework from program.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,39 @@
-#include<iostream>
-#include<fstream>
+include<iostream>
+
 using namespace std;
 
 int main(){
-
-    return 0;
+	
+	cout << "|======MENU======|\n   1. Create an account.\n   2. Use an existing account.   0. Exit the program.\nInput (PASS for admin): ";
+	cin >> userChoice;
+	
+	do {
+		if (userChoice == 0){
+			return 0;
+		}
+		else if (userChoice == 1){
+			accountCreate();
+		}
+		else if (userChoice == 2){
+			cout << "|===ACCOUNT-MENU===|\n   1. Deposit balance.\n   2. Withdraw balance.\n   3. View transaction history.\n   0. Exit menu.\nInput: ";
+			cin >> accountChoice;
+			while (accountChoice != 0){
+				switch (accountChoice) {
+	            case 1:
+	                depositBalance();
+	                break;
+	            case 2:
+	                withdrawBalance();
+	                break;
+	            case 3:
+	                transactionHistory();
+	                break;
+	            default:
+	                cout << "Invalid value entered. Try again." << endl;
+	    	}
+		}
+		else {
+			cout << "Invalid value entered. Try again." << endl;
+		}
+	} while (true);
 }
