f6e4a518f6c1d4794b9ea7f1f8cb0daa743f4ac6
GIKIStudent101 <u2024374@giki.edu.pk>
2024-12-14 16:08:18 +0500

    Finalized framework.

--- main.cpp
+++ main.cpp
@@ -1,39 +1,63 @@
-include<iostream>
+#include<iostream>
 
 using namespace std;
 
 int main(){
 	
-	cout << "|======MENU======|\n   1. Create an account.\n   2. Use an existing account.   0. Exit the program.\nInput (PASS for admin): ";
-	cin >> userChoice;
-	
 	do {
-		if (userChoice == 0){
-			return 0;
-		}
-		else if (userChoice == 1){
+		
+		// Variables = userChoice(int) , userAccount(class)[name, password(not_important_here), balance], accountChoice
+		
+		cout << "|=====MENU=====|" << endl;
+		cout << "   1. Create an account.\n   2. Use an existing account.\n   3. Change bank settings.\n   0. Exit the program.\nInput: ";
+		cin >> userChoice;
+
+		if (userChoice == 1){
 			accountCreate();
 		}
+		
 		else if (userChoice == 2){
-			cout << "|===ACCOUNT-MENU===|\n   1. Deposit balance.\n   2. Withdraw balance.\n   3. View transaction history.\n   0. Exit menu.\nInput: ";
-			cin >> accountChoice;
-			while (accountChoice != 0){
-				switch (accountChoice) {
-	            case 1:
-	                depositBalance();
-	                break;
-	            case 2:
-	                withdrawBalance();
-	                break;
-	            case 3:
-	                transactionHistory();
-	                break;
-	            default:
-	                cout << "Invalid value entered. Try again." << endl;
-	    	}
+			userAccount = accountSelect();
+			
+			do {
+				cout << "|===ACCOUNT-MENU===|" << endl;
+				cout << "   1. Deposit amount.\n   2. Withdraw amount.\n   3. Transfer to another account.\n   0.Exit account settings.\nInput: ";
+				cin >> accountChoice;
+				
+				switch(accountChoice){
+					case 1:
+						accountDeposit();
+						break;
+					case 2:
+						accountWithdraw();
+						break;
+					case 3:
+						accountTransfer();
+						break;
+					case 0:
+						break;
+					default:
+						cout << "Invalid input entered. Try again." << endl;
+						break;
+				}
+				
+				updateAccount();
+				
+			}
+			
 		}
+		
+		else if (userChoice == 3){
+			bankSettings();
+		}
+		
+		else if (userChoice == 0){
+			return 0;
+		}
+		
 		else {
 			cout << "Invalid value entered. Try again." << endl;
 		}
-	} while (true);
+		
+	}
 }
